145f310ea2e0ba9dd22c0379506de9bfffa3a602
Yuri Godke <yurigodke@gmail.com>
2018-04-15 15:31:41 -0300

    Update in list torrent

--- js/list_torrent.js
+++ js/list_torrent.js
@@ -1,18 +1,23 @@
 var htmlSource = document.body.innerHTML;
-var linkRegex = /"magnet:\?([^"].)+"/g;
-var nameRegex = /dn=(([^&].)+)/gi;
+var linkRegex = /(magnet:\?)(([^\"\'])+)/gi;
+var nameRegex = /dn=(([^&].)+)/i;
 
 var links = [];
 var magnetLinks = htmlSource.match(linkRegex);
-for (var i = 0; i < magnetLinks.length; i++) {
-	var magnetLink = magnetLinks[i].replace(/"/g, '');
-	var linkDecode = decodeURIComponent(magnetLink);
-	var titleLink = nameRegex.exec(linkDecode)[1];
 
-	links.push({
-		title: titleLink,
-		link: magnetLink
-	});
+if(magnetLinks){
+	for (var i = 0; i < magnetLinks.length; i++) {
+		var magnetLink = magnetLinks[i];
+		var linkDecode = decodeURIComponent(magnetLink);
+		var titleLink = nameRegex.exec(magnetLink);
+
+		titleLink = titleLink && titleLink[1] ? titleLink[1] : 'Download ' + i;
+
+		links.push({
+			title: titleLink,
+			link: magnetLink
+		});
+	}
 }
 
 links;
