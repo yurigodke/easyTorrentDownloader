a4eae6e250bd0c51161306d7dac4a9be67dca56b
Yuri Godke <yurigodke@gmail.com>
2018-04-15 16:00:37 -0300

    Add remove modal function

--- js/remove_modal.js
+++ js/remove_modal.js
@@ -1,7 +1,17 @@
-var htmlNodes = document.body.querySelectorAll('*');
+HTMLElement.prototype.getZIndex = function () {
+	if (this) {
+		var z = window.document.defaultView.getComputedStyle(this).getPropertyValue('z-index');
+		return Number(z);
+	}
+};
+
+var htmlNodes = document.querySelectorAll('body>*');
 
 for (var i = 0; i < htmlNodes.length; i++) {
-	if (htmlNodes[i].style.zIndex > 0) {
-		console.log(htmlNodes[i]);
+	var zIndex = htmlNodes[i].getZIndex();
+	if (zIndex && zIndex > 100) {
+		htmlNodes[i].remove();
 	}
 }
+
+document.body.style.overflow = 'auto';
